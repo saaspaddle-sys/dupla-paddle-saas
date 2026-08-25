2e1c02e504872da400fe8b433e08c33e3990c0a1
adrián calo <agy.calo90@gmail.com>
2026-08-25 20:18:33 -0300

    Crearcion dashboard customer(clubes),add file documentacion de tareas dashboard-club.md

---
+++ apps/web/src/app/(customers)/dashboard-customer/page.tsx
@@ -0,0 +1,8 @@
+export default function customer(){
+    return (
+    <section className="space-y-2">
+      <h1 className="text-2xl font-black text-deep-onyx">Mi club</h1>
+      <p className="text-sm text-gray-600">Pantalla en construcción.</p>
+    </section>
+  );
+}
--- apps/web/src/app/(customers)/layout.tsx
+++ apps/web/src/app/(customers)/layout.tsx
@@ -1,7 +1,16 @@
 //Sidebar robusto de administración para el Club
 
 import type { ReactNode } from "react";
+import CustomerSidebar from "./_components/customerSidebar";
 
 export default function CustomersLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+      <div className="flex min-h-screen bg-[#f7f9e8]/50">
+        {/* Sidebar Fijo a la izquierda */}
+        <CustomerSidebar />
+  
+        {/* Contenido dinámico (page.tsx) */}
+        <main className="flex-1 p-6 md:p-8 max-w-7xl">{children}</main>
+      </div>
+    );;
 }
